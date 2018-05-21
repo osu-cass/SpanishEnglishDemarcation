b7d44510ff7f21486ec3ee809789f01af74fe198
MatthewArck <matthew.arck@oregonstate.edu>
2018-05-21 11:07:01 -0700

    Initial project creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpanishEnglishDemarcation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
