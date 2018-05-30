4db9506fdcbae62c5b48ff27c60b13d30afa9fd6
Graham Barber <grahammbarber@gmail.com>
2018-05-29 18:09:05 -0700

    refactor(components): comply with React 16, fix with prettier

--- SmarterBalanced.SpanishEnglishDemarcation/ClientApp/components/Counter.tsx
+++ SmarterBalanced.SpanishEnglishDemarcation/ClientApp/components/Counter.tsx
@@ -1,31 +1,44 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
+import * as React from "react";
+import { RouteProps, RouteComponentProps } from "react-router";
 
-interface CounterState {
-    currentCount: number;
+export interface CounterState {
+  currentCount: number;
 }
 
-export class Counter extends React.Component<RouteComponentProps<{}>, CounterState> {
-    constructor() {
-        super();
-        this.state = { currentCount: 0 };
-    }
+export class Counter extends React.Component<
+  RouteComponentProps<{}>,
+  CounterState
+> {
+  constructor(props: RouteComponentProps<{}>) {
+    super(props);
+    this.state = { currentCount: 0 };
+  }
 
-    public render() {
-        return <div>
-            <h1>Counter</h1>
+  public render() {
+    return (
+      <div>
+        <h1>Counter</h1>
 
-            <p>This is a simple example of a React component.</p>
+        <p>This is a simple example of a React component.</p>
 
-            <p>Current count: <strong>{ this.state.currentCount }</strong></p>
+        <p>
+          Current count: <strong>{this.state.currentCount}</strong>
+        </p>
 
-            <button onClick={ () => { this.incrementCounter() } }>Increment</button>
-        </div>;
-    }
+        <button
+          onClick={() => {
+            this.incrementCounter();
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
 
-    incrementCounter() {
-        this.setState({
-            currentCount: this.state.currentCount + 1
-        });
-    }
+  incrementCounter() {
+    this.setState({
+      currentCount: this.state.currentCount + 1
+    });
+  }
 }
--- SmarterBalanced.SpanishEnglishDemarcation/ClientApp/routes.tsx
+++ SmarterBalanced.SpanishEnglishDemarcation/ClientApp/routes.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import * as React from "react";
+import { Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
 
-export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
-</Layout>;
+export const routes = (
+  <Layout>
+    <Route exact path="/" component={home} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/fetchdata" component={FetchData} />
+  </Layout>
+);
