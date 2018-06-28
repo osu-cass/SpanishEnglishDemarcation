7cee7170db572f75c54bfe3682b0f165b48b7b1f
MatthewArck <matthew.arck@oregonstate.edu>
2018-06-28 13:17:49 -0700

    fix: renamed layout pages and added static file handler to startup

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace SmarterBalanced.SpanishEnglishDemarcation
 {
@@ -37,6 +41,11 @@ namespace SmarterBalanced.SpanishEnglishDemarcation
             }
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Content")),
+                RequestPath = "/Content"
+            });
 
             app.UseMvc(routes =>
             {
