cea2674732968b68863710076a784a7d0f3c2b90
tonychen <940044928@qq.com>
2018-06-29 15:49:12 -0700

    feat: update JS and CSS of divided line

---
+++ Content/Scripts/Divided/script.js
@@ -0,0 +1,86 @@
+function Separate_Spanish_English(contents, option) {
+    var Spanish = [];
+    var English = [];
+    for (let i = 0; i < contents.length; i++) {
+        if (contents[i].lang == 'es-mx') {
+            Spanish.push(contents[i]);
+        }
+        else {
+            English.push(contents[i]);
+        }
+    }
+    var Spanish_Block = $('<div></div>');
+    for (let index = 0; index < Spanish.length; index++) {
+        $(Spanish_Block).append(Spanish[index]);
+    }
+    if (option == undefined) {
+        Spanish_Block.addClass("Spanish");
+    }
+    else {
+        Spanish_Block.addClass("Spanish_Answer");
+    }
+
+    var English_Block = $('<div></div>');
+    for (let index = 0; index < English.length; index++) {
+        English_Block.append(English[index]);
+    }
+    if (option == undefined) {
+        English_Block.addClass("English");
+    }
+    else {
+        English_Block.addClass("English_Answer");
+    }
+
+    var New_Passage = $('<div></div>');
+    New_Passage.append(Spanish_Block);
+    New_Passage.append(English_Block);
+    return New_Passage;
+
+}
+
+function Rearrange(name, option) {
+    var Parent = $(name);
+    var Children = Parent.children();
+    var New_Children = Separate_Spanish_English(Children, option);
+    $(Parent).append(New_Children);
+}
+
+Passage_Name = '.thePassage .padding';
+Question_Name = '.stemContainer';
+Choice_Name = '.optionContent';
+if (($(Passage_Name)).length != 0) {
+    Rearrange(Passage_Name);
+}
+else {
+    var Table = $('.bigTable');
+    Table.addClass("center");
+    var Questions = $('.theQuestions');
+    Questions.css("width",'100%');
+}
+if ($(Question_Name) != null) {
+    Rearrange(Question_Name);
+}
+
+var choices = $(Choice_Name);
+if (choices != null) {
+    choices.addClass("Number");
+    var td = $(".tableItem tbody tr td .languagedivider");
+    if (td) {
+        td.prev().addClass("Spanish_Answer");
+        td.next().addClass("English_Answer");
+    }
+}
+
+var options=$(".optionContent .languagedivider");
+if(options){
+    options.prev().addClass("Spanish_Answer");
+    options.next().addClass("English_Answer");
+}
+
+var Answer_Cotainer = $('.answerContainer .tableItem');
+if (Answer_Cotainer != null) {
+    var goal = $('.answerContainer');
+    var width = $(goal).width();
+    $(Answer_Cotainer).css("width",width);
+}
+
