850241c0140a62df94959208f9e4202fb6a4a9b8
tonychen <940044928@qq.com>
2018-06-29 11:16:19 -0700

    fix: update jquery stuff

--- Content/Scripts/Frames/script.js
+++ Content/Scripts/Frames/script.js
@@ -1,102 +1,159 @@
-function Separate_Spanish_English(contents) {
-    var Spanish=[];
-    var English=[];
+function Separate_Spanish_English(contents, option) {
+    var Spanish = [];
+    var English = [];
     for (let i = 0; i < contents.length; i++) {
-        if(contents[i].lang=='es-mx'){
+        if (contents[i].lang == 'es-mx') {
             Spanish.push(contents[i]);
         }
-        else{
+        else {
             English.push(contents[i]);
         }
     }
-   
-    var Spanish_Block=document.createElement("div");
+    var Spanish_Block = $('<div></div>');
     for (let index = 0; index < Spanish.length; index++) {
-        Spanish_Block.appendChild(Spanish[index]);
+        $(Spanish_Block).append(Spanish[index]);
     }
-    Spanish_Block.classList.add("Spanish");
+    if (option == undefined) {
+        Spanish_Block.addClass("Spanish");
+    }
+    else {
+        Spanish_Block.addClass("Spanish_Answer");
+    }
 
-    var English_Block=document.createElement("div");
+    var English_Block = $('<div></div>');
     for (let index = 0; index < English.length; index++) {
-        English_Block.appendChild(English[index]);
+        English_Block.append(English[index]);
     }
-    English_Block.classList.add("English");
-    
-    var New_Passage=document.createElement('div');
-    New_Passage.appendChild(Spanish_Block);
-    New_Passage.appendChild(English_Block);
+    if (option == undefined) {
+        English_Block.addClass("English");
+    }
+    else {
+        English_Block.addClass("English_Answer");
+    }
+
+    var New_Passage = $('<div></div>');
+    New_Passage.append(Spanish_Block);
+    New_Passage.append(English_Block);
     return New_Passage;
- 
+
 }
 
-function Rearrange(name){
-    var Parent=document.querySelector(name);
-    var Children=Parent.children;
-    var New_Children=Separate_Spanish_English(Children);
-    Parent.appendChild(New_Children);
+function Rearrange(name, option) {
+    var Parent = $(name);
+    var Children = Parent.children();
+    var New_Children = Separate_Spanish_English(Children, option);
+    $(Parent).append(New_Children);
 }
 
-function Check_Number(Html_Content){
+function Rearrange_Parent(Parent, option) {
+    var Children = Parent.children();
+    var New_Children = Separate_Spanish_English(Children, option);
+    $(Parent).append(New_Children);
+}
+
+function Answer_Head(arr){
+    for (let i = 0; i < arr.length; i++) {
+        var Children=arr[i].children;
+        var L=Children.length;
+        if(L>=3){
+            var English_icon=document.createElement("i");
+            English_icon.className="fas fa-star";
+            English_icon.classList.add("Icon");
+            arr[i].insertBefore(English_icon,Children[0]);
+            var Spanish_icon=document.createElement("i");
+            Spanish_icon.className="fas fa-square";
+            Spanish_icon.classList.add("Icon");
+            var index=Math.floor(L/2)+1;
+            arr[i].insertBefore(Spanish_icon,Children[index]);
+        }
+    }
+}
+
+function Check_Number(Html_Content) {
     for (let i = 0; i < Html_Content.length; i++) {
-        if((Html_Content[i]<'z' && Html_Content[i]>'a')||(Html_Content[i]<'Z' && Html_Content[i]>'A'))
-        {
+        if ((Html_Content[i] < 'z' && Html_Content[i] > 'a') || (Html_Content[i] < 'Z' && Html_Content[i] > 'A')) {
             return false;
         }
     }
     return true;
 }
-function Check_Img(Child){
-    var nodes=Child.childNodes;
+function Check_Img(Child) {
+    var nodes = Child.childNodes;
     for (let i = 0; i < nodes.length; i++) {
-        if(nodes[i].nodeName=='IMG'){
+        if (nodes[i].nodeName == 'IMG') {
             return true;
         }
     }
     return false;
 }
-Passage_Name='.thePassage .padding';
-Question_Name='.stemContainer';
-Choice_Name='.optionContent';
-if(document.querySelector(Passage_Name)!=null)
-{
+Passage_Name = '.thePassage .padding';
+Question_Name = '.stemContainer';
+Choice_Name = '.optionContent';
+if (document.querySelector(Passage_Name) != null) {
     Rearrange(Passage_Name);
 }
-else{
-    var Table=document.querySelector('.bigTable');
-    Table.classList.add("center");
-    var Questions=document.querySelector('.theQuestions');
-    Questions.style.width='100%';
+else {
+    var Table = $('.bigTable');
+    Table.addClass("center");
+    var Questions = $('.theQuestions');
+    Questions.css("width",'100%');
 }
-if(document.querySelector(Question_Name)!=null)
-{
+if ($(Question_Name) != null) {
     Rearrange(Question_Name);
 }
-var choices=document.querySelectorAll(Choice_Name);
-if(choices!=null)
-{
-    var check=false;
-    if(choices[0]!=null){
-        var Children=choices[0].children;
-        var Html_Content=Children[0].innerHTML;
-        var check=Check_Number(Html_Content)||Check_Img(Children[0]);
-        console.log(check);
+var choices = $(Choice_Name);
+if (choices != null) {
+    choices.addClass("Number");
+    var td = $(".tableItem tbody tr td .languagedivider");
+    if (td) {
+        td.prev().addClass("Spanish_Answer");
+        td.next().addClass("English_Answer");
     }
- 
-    if(check){
-        for (let i = 0; i < choices.length; i++) {
-            choices[i].classList.add('Number');
-        }
+}
+var options=$(".optionContent .languagedivider");
+if(options){
+    options.prev().addClass("Spanish_Answer");
+    options.next().addClass("English_Answer");
+}
+var check=$(".Number");
+//     var check = false;
+//     if (choices[0] != null) {
+//         var Children = choices[0].children;
+//         var Html_Content = Children[0].innerHTML;
+//         var check = Check_Number(Html_Content) || Check_Img(Children[0]);
+//     }
+
+//     if (check) {
+//         for (let i = 0; i < choices.length; i++) {
+//             choices[i].classList.add('Number');
+//         }
+//     }
+//     else {
+//         for (let i = 0; i < choices.length; i++) {
+//             Rearrange('#' + choices[i].id);
+//         }
+//     }
+// }
+var th = $(".tableItem thead tr th .languagedivider");
+if (th) {
+    var pre=th.prevAll();
+    var L=pre.length-1;
+    var next=th.next();
+    var first=$(pre[L]);
+    while(next.html()=="&nbsp")
+    {
+        next=next.next();
     }
-    else{
-        for (let i = 0; i < choices.length; i++) {
-            Rearrange('#'+choices[i].id);
-        }
-    }
+    first.addClass("Spanish_Answer");
+    next.addClass("English_Answer");
 }
 
-var Answer_Cotainer=document.querySelector('.answerContainer .tableItem');
-if(Answer_Cotainer!=null){
-    var goal=document.querySelector('.stemContainer .Spanish');
-    var width=goal.clientWidth;
-    Answer_Cotainer.style.width=width;
+
+var Answer_Cotainer = $('.answerContainer .tableItem');
+if (Answer_Cotainer != null) {
+    var goal = $('.answerContainer');
+    var width = $(goal).width();
+    $(Answer_Cotainer).css("width",width);
 }
+
+
