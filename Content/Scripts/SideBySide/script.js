b62f2a5e89db4da5618160c19178d56ef8dab316
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-08-15 13:38:20 -0700

    style(side-by-side): add comments

--- Content/Scripts/SideBySide/script.js
+++ Content/Scripts/SideBySide/script.js
@@ -1,4 +1,6 @@
+/* Creates passage with separated Spanish & English text blocks */
 function separateSpanishEnglish(contents, option) {
+    // Initialize Spanish & English text arrays //
     var spanish = [];
     var english = [];
     for (let i = 0; i < contents.length; i++) {
@@ -8,6 +10,8 @@ function separateSpanishEnglish(contents, option) {
             english.push(contents[i]);
         }
     }
+
+    // Create div element for Spanish text array //
     var spanishBlock = $('<div></div>');
     for (let index = 0; index < spanish.length; index++) {
         $(spanishBlock).append(spanish[index]);
@@ -18,6 +22,7 @@ function separateSpanishEnglish(contents, option) {
         spanishBlock.addClass("spanish-answer");
     }
 
+    // Create div element for English text array //
     var englishBlock = $('<div></div>');
     for (let index = 0; index < english.length; index++) {
         englishBlock.append(english[index]);
@@ -28,14 +33,15 @@ function separateSpanishEnglish(contents, option) {
         englishBlock.addClass("english-answer");
     }
 
+    // Create div element to hold Spanish & English text blocks //
     var newPassage = $('<div></div>');
     newPassage.append(spanishBlock);
     newPassage.append(englishBlock);
-    $(newPassage).addClass("bigTable");
 
     return newPassage;
 }
 
+/* Takes children elements of a parent element & reorganizes the text */
 function rearrange(name, option) {
     var parent = $(name);
     var children = parent.children();
@@ -43,16 +49,18 @@ function rearrange(name, option) {
     $(parent).append(newChildren);
 }
 
+/* Vars for class setups */
 passageName = '.thePassage .padding';
 questionName = '.stemContainer';
 choiceName = '.optionContent';
 
-if (($(passageName)).length !== 0) {
-    //rearrange(passageName);
-} else {
+if (($(passageName)).length !== 0) {        // rearrange passage if it exists
+    rearrange(passageName);
+} else {                                    // create table for questions if no passage exists
     var table = $('.bigTable');
     table.addClass("center");
     var questions = $('.theQuestions');
+    console.log(table.parent().attr('id'));
     if (table.parent().attr('id')  === 'Item_3635') {
         questions.css("width", '97%');
     } else {
@@ -60,21 +68,49 @@ if (($(passageName)).length !== 0) {
     }
 }
 
-if ($(questionName) != null) {
+/* Rearrange questions based on lang */
+if ($(questionName) !== null) {
     rearrange(questionName);
 }
 
+/* Add answer choices in both langs */
 var choices = $(choiceName);
 if (choices !== null) {
     choices.addClass("number");
+    var td = $(".table-item tbody tr td .languagedivider");
+    if (td) {
+        td.prev().addClass("spanish-answer");
+        td.next().addClass("english-answer");
+    }
 }
 
+/* Add .optionContent options in both langs */
 var options = $(".optionContent .languagedivider");
 if (options) {
     options.prev().addClass("spanish-answer");
     options.next().addClass("english-answer");
 }
 
+/* Add table items in both langs */
+var th = $(".table-item thead tr th .languagedivider");
+if (th) {
+    var pre = th.prevAll();
+    var next = th.nextAll();
+    for (let i = 0; i < pre.length; i++) {
+        var html = ($(pre[i]).html());
+        if(html !== "&nbsp;") {
+            $(pre[i]).addClass('spanish-answer');
+        }
+    }
+    for (let i = 0; i < next.length; i++) {
+        var html = ($(next[i]).html());
+        if (html !== "&nbsp;") {
+            $(next[i]).addClass('english-answer');
+        }
+    }
+}
+
+/*Change width of answer container element */
 var answerContainer = $('.answerContainer .tableItem');
 if (answerContainer !== null) {
     var goal = $('.answerContainer');
@@ -82,10 +118,7 @@ if (answerContainer !== null) {
     $(answerContainer).css("width", width);
 }
 
-$(".stemContainer .spanish").css("width", "45%");
-$(".stemContainer .english").css("width", "45%");
-
-// Container to hold item # and hamburger icon
+/* Container to hold item # and hamburger icon */
 var numContainer = $('<div class="num-container"></div>');
 var questionNumber = $('.questionNumber');
 if (questionNumber) {
