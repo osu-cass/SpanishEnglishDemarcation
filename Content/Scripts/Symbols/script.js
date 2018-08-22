1f29c3c31f7ad4b880bce27d15d095f1f668bdc1
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-08-22 12:44:32 -0700

    fix(symbols): change text from a sans serif to a serif font

--- Content/Scripts/Symbols/script.js
+++ Content/Scripts/Symbols/script.js
@@ -113,6 +113,39 @@ if (answerContainer !== null) {
     $(answerContainer).css("width", width);
 }
 
+/* Changes inline font in tables to a larger serif font */
+function fixTableFont() {
+    const answerHeadings1 = $('table[class="tableItem"] thead tr th p');
+    $.each(answerHeadings1, (idx, val) => {
+        $(val).css('font-family', '')
+            .css('font-family', '\'Times New Roman\', Times, serif !important')
+            .css('font-size', '')
+            .css('font-size', '18px');
+    });
+    const answerHeadings2 = $('table[class="newtable"] thead tr th p');
+    $.each(answerHeadings2, (idx, val) => {
+        $(val).css('font-family', '')
+            .css('font-family', '\'Times New Roman\', Times, serif !important')
+            .css('font-size', '')
+            .css('font-size', '18px');
+    });
+
+    const answerBodyParagraphs1 = $('table[class="tableItem"] tbody tr td p');
+    $.each(answerBodyParagraphs1, (idx, val) => {
+        $(val).css('font-family', '')
+            .css('font-size', '')
+            .addClass('table-text');
+    });
+    const answerBodyParagraphs2 = $('table[class="newtable"] tbody tr td p');
+    $.each(answerBodyParagraphs2, (idx, val) => {
+        $(val).css('font-family', '')
+            .css('font-size', '')
+            .addClass('table-text');
+    });
+}
+
+fixTableFont();
+
 /* Container to hold item # and hamburger icon */
 var numContainer = $('<div class="num-container"></div>');
 var questionNumber = $('.questionNumber');
