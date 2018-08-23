6117e09e345d07d5698f0fa4f3d802689983a023
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-08-23 09:56:53 -0700

    refactor(stacked): remove obsolete functions, align radio button content

--- Content/Scripts/Stacked/script.js
+++ Content/Scripts/Stacked/script.js
@@ -146,43 +146,47 @@ function wrapNumHamburger() {
     $(questions).prepend(numContainer);              // add container to questions
 }
 
-function fixTableFont() {
-    /* Change inline font in tables to a larger serif font */
-    const answerHeadings1 = $('table[class="tableItem"] thead tr th p');
-    $.each(answerHeadings1, (idx, val) => {
-        $(val).css('font-family', '')
-            .css('font-family', '\'Times New Roman\', Times, serif !important')
-            .css('font-size', '')
-            .css('font-size', '18px');
-    });
-    const answerHeadings2 = $('table[class="newtable"] thead tr th p');
-    $.each(answerHeadings2, (idx, val) => {
-        $(val).css('font-family', '')
-            .css('font-family', '\'Times New Roman\', Times, serif !important')
-            .css('font-size', '')
-            .css('font-size', '18px');
-    });
 
-    const answerBodyParagraphs1 = $('table[class="tableItem"] tbody tr td p');
-    $.each(answerBodyParagraphs1, (idx, val) => {
-        $(val).css('font-family', '')
-            .css('font-size', '')
-            .addClass('table-text');
-    });
-    const answerBodyParagraphs2 = $('table[class="newtable"] tbody tr td p');
-    $.each(answerBodyParagraphs2, (idx, val) => {
-        $(val).css('font-family', '')
-            .css('font-size', '')
-            .addClass('table-text');
-    });
-}
 
-sortColumns();
-createQuestions();
-wrapNumHamburger();
-fixTableFont();
+
+
+/* Change inline font in tables to a larger serif font */
+const answerHeadings1 = $('table[class="tableItem"] thead tr th p');
+$.each(answerHeadings1, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-family', '\'Times New Roman\', Times, serif !important')
+        .css('font-size', '')
+        .css('font-size', '18px');
+});
+const answerHeadings2 = $('table[class="newtable"] thead tr th p');
+$.each(answerHeadings2, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-family', '\'Times New Roman\', Times, serif !important')
+        .css('font-size', '')
+        .css('font-size', '18px');
+});
+
+const answerBodyParagraphs1 = $('table[class="tableItem"] tbody tr td p');
+$.each(answerBodyParagraphs1, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-size', '')
+        .addClass('table-text');
+});
+const answerBodyParagraphs2 = $('table[class="newtable"] tbody tr td p');
+$.each(answerBodyParagraphs2, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-size', '')
+        .addClass('table-text');
+});
+
+
 
 /* Eliminate extra spacing in titles */
 if ($('.spanish h2 p[lang="es-mx"]')) {
     $('.spanish h2 p[lang="es-mx"]').remove();
 }
+
+
+
+/* Align radio buttons with answer content */
+const answers = $('.optionContainer').css('display', 'flex').css('align-items', 'center');
