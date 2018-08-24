9c4b421a99cd98ddd1941393970abb855f5a43fa
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-08-24 14:49:20 -0700

    fix(divided frames): change table font to serif

--- Content/Scripts/DividedFrames/script.js
+++ Content/Scripts/DividedFrames/script.js
@@ -176,3 +176,34 @@ if ($(englishTableFootnote).attr('colspan') > 1) {
         .css('width', '40%')
         .css('border-top', '2px solid black');
 }
+
+
+
+/* Changes inline font in tables to a larger serif font */
+const answerHeadings1 = $('table[class="tableItem"] thead tr th p');
+$.each(answerHeadings1, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-family', '\'Times New Roman\', Times, serif !important')
+        .css('font-size', '')
+        .css('font-size', '18px');
+});
+const answerHeadings2 = $('table[class="newtable"] thead tr th p');
+$.each(answerHeadings2, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-family', '\'Times New Roman\', Times, serif !important')
+        .css('font-size', '')
+        .css('font-size', '18px');
+});
+
+const answerBodyParagraphs1 = $('table[class="tableItem"] tbody tr td p');
+$.each(answerBodyParagraphs1, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-size', '')
+        .addClass('table-text');
+});
+const answerBodyParagraphs2 = $('table[class="newtable"] tbody tr td p');
+$.each(answerBodyParagraphs2, (idx, val) => {
+    $(val).css('font-family', '')
+        .css('font-size', '')
+        .addClass('table-text');
+});
